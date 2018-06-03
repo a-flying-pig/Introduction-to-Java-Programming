9c1be111e656124bae984122b51f77f0763d7803
HuaZhu <zhuhualoyal@163.com>
2018-06-03 22:26:16 +0800

    add codes that writed at home

---
+++ src/capture23/GCD2.java
@@ -0,0 +1,26 @@
+package capture23;
+
+import java.util.Scanner;
+
+public class GCD2 {
+	public static int gcd (int m, int n) {
+		if (m % n == 0) 
+			return n;
+		else
+			return gcd(n, m % n);
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter first integer: ");
+		int m = input.nextInt();
+		System.out.println("Enter second interger: ");
+		int n = input.nextInt();
+		
+		input.close();
+		
+		System.out.println("The greatest common divisor for " + m + 
+				" and " + n + " is " + gcd(m, n));
+	}
+}
